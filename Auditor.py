f2e67a11186e15d9eaeb76bddfe8bd71491a5988
shine06.a@gmail.com <shine06.a@gmail.com>
2026-09-11 16:17:59 +0800

    loop

--- Auditor.py
+++ Auditor.py
@@ -7,29 +7,3 @@ while True:
 
     if stock_q.lower() == "quit":
         break
-
-    if not stock_q.isdigit():
-        print("Error: Please enter a valid integer.")
-        failed += 1
-        continue
-
-    stock_q = int(stock_q)
-    inventory += stock_q
-
-    if inventory > 500:
-        print("ALERT: Inventory exceeds 500 units!")
-        break
-
-
-print("\n--- Final Report ---")
-print("Total Units Processed:", inventory)
-print("Number of Failed/Rejected Entries:", failed)
-    
-
-
-   
-
-
-
-    
-
