0176f3e9863f263de5af0bbe5a41cc1206b9d2c6
shine06.a@gmail.com <shine06.a@gmail.com>
2026-09-18 14:39:32 +0800

    caculate tax

--- modular_auditor.py
+++ modular_auditor.py
@@ -13,13 +13,19 @@ def get_valid_input():
 
     return stock_q
 
-
 def process_delivery(current_total, new_value)
     current_total += new_value
     if current_total > 500:
         print("ALERT: Inventory exceeds 500 units!")
         return current_total, True
     return current_total, False
+
+def caculate_tax(amount):
+     tax_rate = 0.10 
+     tax_amount = amount * tax_rate
+     return tax_amount     
+
+    
 
 
 
