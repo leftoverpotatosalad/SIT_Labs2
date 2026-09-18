bd0d83a7919ec77b8c3c2e36913c8198064c73b9
shine06.a@gmail.com <shine06.a@gmail.com>
2026-09-18 14:37:55 +0800

    get vaild input

---
+++ modular_auditor.py
@@ -0,0 +1,54 @@
+def get_valid_input():
+    stock_q = input("Enter stock quantity (or 'quit' to exit): ")
+    if stock_q.lower == "quit":
+        return "quit"
+    if not stock_q.isdigit():
+            print("Error: Please enter a valid integer.")
+            return None
+    stock_q = int(stock_q)
+
+    if stock_q < 0:
+         print("Please enter a vaild postive integer")
+         Return None
+
+    return stock_q
+
+
+    
+
+
+
+"""
+inventory = 0
+stock_q = 0
+failed = 0
+
+
+while True:
+    
+
+    if stock_q.lower() == "quit":
+        break
+
+
+    if not stock_q.isdigit():
+        print("Error: Please enter a valid integer.")
+        failed += 1
+        continue
+
+    stock_q = int(stock_q)
+    if stock_q < 0: 
+        print("Error: Please enter a Postive number")
+        failed += 1
+        continue
+
+    Inventory += stock_q
+
+    if Inventory > 500:
+        print("ALERT: Inventory exceeds 500 units!")
+        break
+
+print("\n--- Final Report ---")
+print("Total Units Processed:", Inventory)
+print("Number of Failed/Rejected Entries:", failed)
+"""
